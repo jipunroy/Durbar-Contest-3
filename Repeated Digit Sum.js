9f97be165972da0aeef4581ea1d6950f6ad31272
jipunroy95 <jipunroy95@gmail.com>
2026-09-13 10:25:24 +0600

    Repeated Digit Sum

---
+++ Repeated Digit Sum.js
@@ -0,0 +1,15 @@
+// Repeated Digit Sum
+function repeatedDigitSum(n) {
+  while (n >= 10) {
+    let sum = 0;
+
+    while (n > 0) {
+      sum += n % 10;
+      n = Math.floor(n / 10);
+    }
+
+    n = sum;
+  }
+
+  return n;
+}
